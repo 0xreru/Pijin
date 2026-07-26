ca0b56557752be4d966b9b6fb84ab1bd97c6866f
xreru <quiarorojanrell@gmail.com>
2026-07-26 23:00:37 +0800

    feat: add banner

--- apps/backend/app/page.tsx
+++ apps/backend/app/page.tsx
@@ -7,6 +7,7 @@ import {
   ProductTour,
   MeetTheTeam,
   FAQ,
+  Banner,
   Footer,
 } from './components';
 
@@ -18,6 +19,7 @@ export default function HomePage() {
       <ProductTour />
       <MeetTheTeam />
       <FAQ />
+      <Banner />
       <Footer />
     </Layout>
   );
